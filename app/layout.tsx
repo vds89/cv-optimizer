b7599aa1b1a7d152dbd3ee0450251d82520c53c6
vds89 <vincenzodesimone04@gmail.com>
2024-12-21 16:53:18 +0100

    Deployment bug correction_v1

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { EdgeStoreProvider } from '../app/lib/edgestore';
 import "./globals.css";
+import { Roboto } from 'next/font/google';
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,11 +21,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={roboto.className}>
         <EdgeStoreProvider>{children}</EdgeStoreProvider>
-        
       </body>
     </html>
   );
